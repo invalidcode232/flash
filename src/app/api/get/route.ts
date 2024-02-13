13bd26703bbeab06cae34083797b852c927ef74a
invalidcode <invalidcode69@gmail.com>
2024-02-13 17:03:43 +0700

    feat: add get flashcard router

---
+++ src/app/api/get/route.ts
@@ -0,0 +1,21 @@
+import prisma from "@/lib/prisma";
+
+export async function GET(req: Request) {
+  const flashcard = await prisma.flashcard.findFirst({
+    include: {
+      choices: true,
+    },
+    where: {
+      next_date: {
+        lte: new Date(),
+      },
+    },
+    orderBy: {
+      next_date: "asc",
+    },
+  });
+
+  console.log(flashcard);
+
+  return new Response(JSON.stringify(flashcard));
+}
