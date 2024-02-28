99ca224db1b62cca0668963decb9dbf882f4ac15
invalidcode <invalidcode69@gmail.com>
2024-02-28 20:53:06 +0700

    fix: change update date logic

--- src/app/api/flashcards/answer/route.ts
+++ src/app/api/flashcards/answer/route.ts
@@ -1,11 +1,20 @@
 import prisma from "@/lib/prisma";
+import { Difficulty } from "@/lib/utils";
 
 export async function POST(req: Request) {
   const { isCorrect, flashcardId, difficulty } = await req.json();
 
   const nextDate = new Date();
   if (isCorrect) {
-    nextDate.setDate(nextDate.getDate() + difficulty);
+    if (difficulty === Difficulty.EASY) {
+      nextDate.setDate(nextDate.getDate() + 3);
+    }
+    if (difficulty === Difficulty.MEDIUM) {
+      nextDate.setDate(nextDate.getDate() + 1);
+    }
+    if (difficulty === Difficulty.HARD) {
+      nextDate.setHours(nextDate.getHours() + 6);
+    }
   } else {
     nextDate.setDate(nextDate.getDate() + 1);
   }
